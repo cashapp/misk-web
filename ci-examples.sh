9d55dda21433051f7abe7d4e16675360720cbb01
Andrew Alexander <account+github@adrw.ch>
2018-11-27 11:25:54 -0500

    Example Services

--- ci-examples.sh
+++ ci-examples.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-for dir in "$(pwd)/"/examples/*/src/
+for dir in "$(pwd)/"/examples/services/*/src/
+do
+    dir=${dir%*/src/}
+    slug=$(basename $dir)
+    docker run  --rm --name "$(date '+%Y-%m-%d%H-%M-%S')-$slug" -v "$dir:/web/tabs/$slug"  squareup/misk-web:latest /bin/misk-web -g
+done
+
+for dir in "$(pwd)/"/examples/tabs/*/src/
 do
     dir=${dir%*/src/}
     slug=$(basename $dir)
