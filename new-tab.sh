93563727b483b2a31dc6885a8dc84b08f849c925
Andrew (Paradi) Alexander <account+github@adrw.ch>
2018-12-05 11:38:04 -0500

    Change from tgz to tar. New-Tab.sh Steps 1-4 (#63)

--- new-tab.sh
+++ new-tab.sh
@@ -1,21 +1,42 @@
-#!/bin/sh
+#!/bin/bash
 
+OLD_ALL_CAP_CASE="PALETTE"
+OLD_TITLE_CASE="Palette"
+OLD_CAMEL_CASE="palette"
+OLD_SLUG_CASE="palette"
 
-# 1. Make it work through `curl | bash -s` so shell file doesn't have to be updated
+# 1. Make it work through `curl | bash -s` so a local shell file doesn't have to be updated
 
 
 # 1. Prompt for new name in formats: `foo-bar`, `fooBar`, `FooBar`
-
+echo "Misk-Web: New Tab"
+echo "A new tab will be created in $(pwd)/{new-tab}. If this is not right the directory, re-run this command in the correct directory."
+echo "You will now be guided through the steps to create a new tab based on the exemplar Palette tab"
+echo "Palette Tab: https://github.com/square/misk-web/tree/master/examples/tabs/palette"
+echo "New Tab Name"
+echo "You'll now be asked how you want your tab name written in different formats (ALLCAPCASE, TitleCase, camelCase, slug-case)."
+read -p "Tab Name in ALLCAPCASE: " -r NEW_ALL_CAP_CASE
+read -p "Tab Name in TitleCase: " -r NEW_TITLE_CASE
+read -p "Tab Name in camelCase: " -r NEW_CAMEL_CASE
+read -p "Tab Name in slug-case: " -r NEW_SLUG_CASE
+echo ""
+echo "Registered NEW_ALL_CAP_CASE   ${NEW_ALL_CAP_CASE}"
+echo "Registered NEW_TITLE_CASE     ${NEW_TITLE_CASE}"
+echo "Registered NEW_CAMEL_CASE     ${NEW_CAMEL_CASE}"
+echo "Registered NEW_SLUG_CASE      ${NEW_SLUG_CASE}"
 
 # 1. Download .zip of palette src code from github
-curl -s0 https://github.com/square/misk-web/raw/master/examples/tabs/palette/src.tgz -o ./src.tgz
-
+echo "Downloading fresh Palette tab"
+rm ./palette.tar
+curl -s0 https://github.com/square/misk-web/raw/master/examples/tabs/palette/palette.tar -o ./palette.tar
 
 # 1. Unzips to pwd
-
+echo "Unzip compressed Palette Tab"
+tar -xvf ./palette.tar
 
 # 1. Renames folder to `foo-bar
-
+echo "Moving Palette source into new tab directory: ${NEW_SLUG_CASE}"
+mv ${OLD_SLUG_CASE} ${NEW_SLUG_CASE}
 
 # 1. [Recursive find/replace](https://stackoverflow.com/questions/11392478/how-to-replace-a-string-in-multiple-files-in-linux-command-line) in directory of `palette` -> `fooBar`, `Palette` -> `FooBar`
 
