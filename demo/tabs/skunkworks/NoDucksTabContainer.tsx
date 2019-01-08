a3e8e72ea848a32c4fb16df993ea8c508be35d91
Andrew Alexander <account+github@adrw.ch>
2019-01-08 14:49:17 -0500

    [#10] Migrate examples/tabs to Rush

---
+++ demo/tabs/skunkworks/NoDucksTabContainer.tsx
@@ -0,0 +1,39 @@
+import { get, initialResponseState, OfflineComponent } from "@misk/core"
+import * as React from "react"
+import { SampleTableComponent } from "../components"
+
+const shortUrlsUrl =
+  "https://square.github.io/misk-web/examples/data/demo/shortUrls.json"
+
+class TabContainer extends React.PureComponent<any> {
+  state = {
+    shortUrls: initialResponseState()
+  }
+
+  async componentDidMount() {
+    this.setState({
+      ...this.state,
+      shortUrls: await get(shortUrlsUrl)
+    })
+  }
+
+  render() {
+    if (this.state.shortUrls.data) {
+      return (
+        <SampleTableComponent
+          data={this.state.shortUrls.data.urlTokenMetadata}
+        />
+      )
+    } else {
+      return (
+        <OfflineComponent
+          title={"Error Loading Palette Short Urls"}
+          error={this.state.shortUrls.error}
+          endpoint={shortUrlsUrl}
+        />
+      )
+    }
+  }
+}
+
+export default TabContainer
