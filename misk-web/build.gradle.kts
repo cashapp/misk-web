895e39a343b794dd324ee5ec56689a5fa33aa58f
Andrew Alexander <account+github@adrw.ch>
2019-01-16 16:34:19 -0500

    [#8] Gradle Misk-Web-Plugin
    * Official `misk-web-plugin` to replace `apply from: "http://github.com/.../web.gradle"` in downstream services
    * Move Travis CI from `oraclejdk8/9` to `openjdk11`
    * [#113] Minimize dependencies in `@misk/cli` to keep binary sizes smaller
    * Migrate all build files to Gradle Kotlin DSL

---
+++ misk-web/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+  id("misk-web-plugin") version "1.0"
+  `kotlin-dsl`
+  `maven-publish`
+}
+
+sourceSets {
+  main {
+    resources {
+      setSrcDirs(listOf(
+          "web/packages/@misk/common/lib",
+          "web/packages/@misk/core/lib",
+          "web/packages/@misk/dev/lib",
+          "web/packages/@misk/tslint/lib"
+      ))
+      exclude("**/node_modules")
+    }
+  }
+}
+
+publishing {
+  repositories {
+    maven(url = "build/repository")
+  }
+}
+
+if (rootProject.file("hooks.gradle").exists()) {
+  apply(from = rootProject.file("hooks.gradle"))
+}
