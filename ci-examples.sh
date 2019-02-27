920b8fe21bf41f0f8b3c2849557a46effbbfa840
Andrew (Paradi) Alexander <account+github@adrw.ch>
2019-02-27 13:39:11 -0500

    Move @misk/ packages out of Gradle managed subproject. Remove Docker. (#215)
    * Move @misk/ packages out of Gradle managed subproject
    
    * Remove docker-related misk-web files since they're no longer used

--- ci-examples.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for dir in "$(pwd)/"examples/*/*/src/
-do
-    dir=${dir%*/src/}
-    slug=$(basename $dir)
-    echo "[CI-EXAMPLES] ${dir} - ${slug}"
-    docker run  --rm --name "$(date '+%Y-%m-%d%H-%M-%S')-$slug" -v "$dir:/web/tabs/$slug"  squareup/misk-web:latest /bin/misk-web -g
-done
