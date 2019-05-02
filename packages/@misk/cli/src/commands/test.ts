6fa2052d3baf7aa94efc00ccb6857b1e87964fb5
Andrew Alexander <account+github@adrw.ch>
2019-05-02 16:24:18 -0400

    [CLI] Add test command

---
+++ packages/@misk/cli/src/commands/test.ts
@@ -0,0 +1,8 @@
+import { logDebug, execute, handleCommand, npmRunScript } from "../utils"
+export const command = "test"
+export const desc = "run tests"
+export const handlerFn = async (...args: any) => {
+  logDebug(command, desc)
+  execute(npmRunScript(command, true), ...args)
+}
+export const handler = async (yargs: any) => handleCommand(yargs, handlerFn)
