8d2fae0887fdac2a26647d64072a6ebee8f1c47c
Andrew Alexander <adrw@squareup.com>
2019-08-27 19:42:24 -0400

    Add gh-pages as ignored branch for CircleCI

--- docs/refresh-external.sh
+++ docs/refresh-external.sh
@@ -30,7 +30,3 @@ for dir in $MW/packages/@misk/*/; do
   PKG=${dir##*/}    # print everything after the final "/"
   cp $dir/*.md $MW/docs/docs/packages/$PKG/
 done
-
-# Copy over CircleCI to ensure CI still works on gh-pages branch
-mkdir -p $MW/docs/public
-cp -r $MW/.circleci $MW/docs/public/.circleci
